ab10242a2b2a9e979447859f731c634594984b30
Victor Porton <porton@narod.ru>
2020-10-27 14:04:47 +0200

    created some files

---
+++ example/zrc5/transfer.js
@@ -0,0 +1,56 @@
+const {BN, Long, bytes, units} = require('@zilliqa-js/util');
+const {Zilliqa} = require('@zilliqa-js/zilliqa');
+const {
+    toBech32Address,
+    getAddressFromPrivateKey,
+} = require('@zilliqa-js/crypto');
+
+
+async function main() {
+    const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
+    const CHAIN_ID = 333;
+    const MSG_VERSION = 1;
+    const VERSION = bytes.pack(CHAIN_ID, MSG_VERSION);
+    privkey = '07e0b1d1870a0ba1b60311323cb9c198d6f6193b2219381c189afab3f5ac41a9';
+    zilliqa.wallet.addByPrivateKey(
+        privkey
+    );
+    const address = getAddressFromPrivateKey(privkey);
+    console.log("Your account address is:");
+    console.log(`${address}`);
+    const myGasPrice = units.toQa('1000', units.Units.Li); // Gas Price that will be used by all transactions
+
+
+    const ftAddr = toBech32Address("509ae6e5d91cee3c6571dcd04aa08288a29d563a");
+    try {
+        const contract = zilliqa.contracts.at(ftAddr);
+        const callTx = await contract.call(
+            'Transfer',
+            [
+                {
+                    vname: 'to',
+                    type: 'ByStr20',
+                    value: "0xBFe2445408C51CD8Ee6727541195b02c891109ee",
+                },
+                {
+                    vname: 'amount',
+                    type: 'Uint128',
+                    value: "100",
+                }
+            ],
+            {
+                // amount, gasPrice and gasLimit must be explicitly provided
+                version: VERSION,
+                amount: new BN(0),
+                gasPrice: myGasPrice,
+                gasLimit: Long.fromNumber(10000),
+            }
+        );
+        console.log(JSON.stringify(callTx.receipt, null, 4));
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
