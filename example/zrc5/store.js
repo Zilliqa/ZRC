978eedff015e8e1f64ab40453a04e9979dd38d00
Victor Porton <porton@narod.ru>
2020-10-27 18:06:46 +0200

    a test almost finished

--- example/zrc5/store.js
+++ example/zrc5/store.js
@@ -21,16 +21,46 @@ async function main() {
     const myGasPrice = units.toQa('1000', units.Units.Li); // Gas Price that will be used by all transactions
 
 
-    const ftAddr = toBech32Address("9b80a12a68989575b7fd4b82a6dabee468ab9d92");
+    const ftAddrHuman = "509ae6e5d91cee3c6571dcd04aa08288a29d563a";
+    const mtAddrHuman = "9b80a12a68989575b7fd4b82a6dabee468ab9d92";
+    const ftAddr = toBech32Address('0x'+ftAddrHuman);
+    const mtAddr = toBech32Address('0x'+mtAddrHuman);
+    const receiptAddrHuman = "BFe2445408C51CD8Ee6727541195b02c891109ee";
+
     try {
-        const contract = zilliqa.contracts.at(ftAddr);
-        const callTx = await contract.call(
+        const ftContract = zilliqa.contracts.at(ftAddr);
+        const mtContract = zilliqa.contracts.at(mtAddr);
+
+        const createTokenCallTx = await mtContract.call(
+            'Transfer',
+            [
+                {
+                    vname: 'CreateZRC2BridgeToken',
+                    type: 'ByStr20',
+                    value: '0x'+ftAddrHuman,
+                }
+            ],
+            {
+                // amount, gasPrice and gasLimit must be explicitly provided
+                version: VERSION,
+                amount: new BN(0),
+                gasPrice: myGasPrice,
+                gasLimit: Long.fromNumber(10000),
+            }
+        );
+        console.log(JSON.stringify(createTokenCallTx/*.receipt*/, null, 4));
+        return;
+
+        const confirmedCreateTokenCallTx = await createTokenCallTx.confirm(createTokenCallTx.id);
+        console.log(JSON.stringify(confirmedCreateTokenCallTx.receipt, null, 4));
+
+        const depositCallTx = await ftContract.call(
             'Transfer',
             [
                 {
                     vname: 'to',
                     type: 'ByStr20',
-                    value: "0xBFe2445408C51CD8Ee6727541195b02c891109ee",
+                    value: mtAddr,
                 },
                 {
                     vname: 'amount',
@@ -46,7 +76,65 @@ async function main() {
                 gasLimit: Long.fromNumber(10000),
             }
         );
-        console.log(JSON.stringify(callTx.receipt, null, 4));
+        console.log(JSON.stringify(depositCallTx.receipt, null, 4));
+
+        const transferCallTx = await mtContract.call(
+            'Transfer',
+            [
+                {
+                    vname: 'token',
+                    type: 'Uint64',
+                    value: ??,
+                },
+                {
+                    vname: 'to',
+                    type: 'ByStr20',
+                    value: receiptAddrHuman,
+                },
+                {
+                    vname: 'amount',
+                    type: 'Uint128',
+                    value: "100",
+                }
+            ],
+            {
+                // amount, gasPrice and gasLimit must be explicitly provided
+                version: VERSION,
+                amount: new BN(0),
+                gasPrice: myGasPrice,
+                gasLimit: Long.fromNumber(10000),
+            }
+        );
+        console.log(JSON.stringify(transferCallTx.receipt, null, 4));
+
+        const withdrawCallTx = await mtContract.call(
+            'Withdraw',
+            [
+                {
+                    vname: 'token',
+                    type: 'Uint64',
+                    value: ??,
+                },
+                {
+                    vname: 'to',
+                    type: 'ByStr20',
+                    value: receiptAddrHuman,
+                },
+                {
+                    vname: 'amount',
+                    type: 'Uint128',
+                    value: "100",
+                }
+            ],
+            {
+                // amount, gasPrice and gasLimit must be explicitly provided
+                version: VERSION,
+                amount: new BN(0),
+                gasPrice: myGasPrice,
+                gasLimit: Long.fromNumber(10000),
+            }
+        );
+        console.log(JSON.stringify(transferCallTx.receipt, null, 4));
 
     } catch (err) {
         console.log(err);
