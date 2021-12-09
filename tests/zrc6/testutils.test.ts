07339066d3275e2d2343ba6234d7851d11ed1ff1
noel <noel@zilliqa.com>
2021-12-09 22:33:18 +0900

    fix: ci

--- tests/zrc6/testutils.test.ts
+++ tests/zrc6/testutils.test.ts
@@ -4,7 +4,7 @@ describe("extractTypes", () => {
   const testCases = [
     {
       type: "Option (ByStr20)",
-      want: ["ByStr21"],
+      want: ["ByStr20"],
     },
     {
       type: "Pair (ByStr20) (Uint256)",
